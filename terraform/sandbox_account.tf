51981e33dd175539617d100272e2d37c1a20a24d
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-20 18:42:31 -0500

    Now we can create accounts by just modifying tfvars file

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -5,20 +5,5 @@ module "sandbox_account" {
   control_tower_parameters = each.value.control_tower_parameters
   account_tags = each.value.account_tags
   change_management_parameters = each.value.change_management_parameters
-  /*
-  control_tower_parameters = {
-      AccountEmail              = each.value.control_tower_parameters.AccountEmail
-      AccountName               = each.value.control_tower_parameters.AccountName
-      ManagedOrganizationalUnit = each.value.control_tower_parameters.ManagedOrganizationalUnit
-      SSOUserEmail              = each.value.control_tower_parameters.SSOUserEmail
-      SSOUserFirstName          = each.value.control_tower_parameters.SSOUserFirstName
-      SSOUserLastName           = each.value.control_tower_parameters.SSOUserLastName
-    }
-    account_tags = each.value.account_tags
-  
-    change_management_parameters = {
-        change_requested_by = each.value.change_management_parameters.change_requested_by
-        change_reason       = each.value.change_management_parameters.change_reason
-    }*/
-      account_customizations_name = "sandbox-customizations"
+  account_customizations_name = "sandbox-customizations"
 }
