c29e5d595730c4f9fbe20276bb15c3e93a0670c5
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-20 18:30:51 -0500

    DB Table error

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -2,9 +2,18 @@ module "sandbox_account" {
   source = "./modules/aft-account-request"
   for_each = {for i, account in var.accounts : i => account}
 
-  control_tower_parameters = each.value.control_tower_parameters
-
-  account_tags = each.value.account_tags
-
-  change_management_parameters = each.value.change_management_parameters
+  control_tower_parameters = {
+      AccountEmail              = each.value.control_tower_parameters.AccountEmail
+      AccountName               = each.value.control_tower_parameters.AccountName
+      ManagedOrganizationalUnit = each.value.control_tower_parameters.ManagedOrganizationalUnit
+      SSOUserEmail              = each.value.control_tower_parameters.SSOUserEmail
+      SSOUserFirstName          = each.value.control_tower_parameters.SSOUserFirstName
+      SSOUserLastName           = each.value.control_tower_parameters.SSOUserLastName
+    }
+    account_tags = each.value.account_tags
+  
+    change_management_parameters = {
+        change_requested_by = each.value.change_management_parameters.change_requested_by
+        change_reason       = each.value.change_management_parameters.change_reason
+    }
 }
