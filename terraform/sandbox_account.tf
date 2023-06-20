cac9c7bf0811d5729355908710dd71360b6aa03c
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-20 18:39:16 -0500

    Works! Now let's make it easier to use

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -2,6 +2,10 @@ module "sandbox_account" {
   source = "./modules/aft-account-request"
   for_each = {for i, account in var.accounts : i => account}
 
+  control_tower_parameters = each.value.control_tower_parameters
+  account_tags = each.value.account_tags
+  change_management_parameters = each.value.change_management_parameters
+  /*
   control_tower_parameters = {
       AccountEmail              = each.value.control_tower_parameters.AccountEmail
       AccountName               = each.value.control_tower_parameters.AccountName
@@ -15,6 +19,6 @@ module "sandbox_account" {
     change_management_parameters = {
         change_requested_by = each.value.change_management_parameters.change_requested_by
         change_reason       = each.value.change_management_parameters.change_reason
-    }
+    }*/
       account_customizations_name = "sandbox-customizations"
 }
