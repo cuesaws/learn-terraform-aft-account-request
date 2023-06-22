92ffbd9da6e185f8936b11e89c8161266cefcf98
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-22 15:02:38 -0500

    Account creation2

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -5,5 +5,6 @@ module "sandbox_account" {
   control_tower_parameters = each.value.control_tower_parameters
   account_tags = each.value.account_tags
   change_management_parameters = each.value.change_management_parameters
-  account_customizations_name = "sandbox-customizations"
+  account_customizations_name = "sandbox-customizations-${each.key}"
 }
+
