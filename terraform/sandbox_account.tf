569c55f5439730f02860cdbf07f07f8522b8c45d
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-22 15:06:05 -0500

    Account creation2

--- terraform/sandbox_account.tf
+++ terraform/sandbox_account.tf
@@ -1,7 +1,6 @@
 module "sandbox_account" {
   source = "./modules/aft-account-request"
   for_each = {for i, account in var.accounts : i => account}
-
   control_tower_parameters = each.value.control_tower_parameters
   account_tags = each.value.account_tags
   change_management_parameters = each.value.change_management_parameters
