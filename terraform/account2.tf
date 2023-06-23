4884f23710a770b70cf87bf0617c7c7b0fa316c7
Jacob Sanchez <jacob.sanchez@slalom.com>
2023-06-23 04:18:55 -0500

    Email Exist error

--- terraform/account2.tf
+++ terraform/account2.tf
@@ -2,10 +2,10 @@ module "sandbox_account02" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-      AccountEmail              = "sanchezjacob4203@gmail.com"
+      AccountEmail              = "sanchezjacob244@gmail.com"
       AccountName               = "sandbox-account02"
       ManagedOrganizationalUnit = "AFT (ou-dld9-73newpki)"
-      SSOUserEmail              = "sanchezjacob4203@gmail.com"
+      SSOUserEmail              = "sanchezjacob244@gmail.com"
       SSOUserFirstName          = "Jacob"
       SSOUserLastName           = "Sanchez"
     }
@@ -13,7 +13,7 @@ module "sandbox_account02" {
   account_tags = {
     "application" = "AFT"
     "business_region" = "us-east-1"
-    "owner" = "jacob.sanchez@slalom.com"
+    "owner" = "sanchezjacob244@gmail.com"
   }
   
   change_management_parameters = {
