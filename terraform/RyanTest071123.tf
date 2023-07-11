f5577b76e79c981caadc287e553cf015974c6dab
cuesaws <132401293+cuesaws@users.noreply.github.com>
2023-07-11 14:44:30 -0400

    New account creation- Ryan Test

--- terraform/RyanTest071123.tf
+++ terraform/RyanTest071123.tf
@@ -1,24 +1,24 @@
-module "ryan_test_account" {
+/*module "RyanTestAccount071123" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-      AccountEmail              = "ryan.haughton@gmail.com"
-      AccountName               = "ryan_test_account"
+      AccountEmail              = "rhaughton@cuesinc.com"
+      AccountName               = "Ryan_FL"
       ManagedOrganizationalUnit = "AFT (ou-dld9-73newpki)"
-      SSOUserEmail              = "ryan.haughton@gmail.com"
+      SSOUserEmail              = "rhaughton@cuesinc.com"
       SSOUserFirstName          = "Ryan"
       SSOUserLastName           = "Haughton"
     }
   account_tags = {
     "application" = "AFT"
     "business_region" = "us-east-1"
-    "owner" = "ryan.haughton@gmail.com"
+    "owner" = "rhaughton@cuesinc.com"
   }
   
   change_management_parameters = {
     change_requested_by = "Ryan Haughton"
-    change_reason       = "AWS Control Tower Account Factory for Terraform test run by RH"
+    change_reason       = "New test account by RH"
   }
 
   account_customizations_name = "sandbox"
-}
+}*/
