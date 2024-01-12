b7e36ed10cea898640261236399f48a7ecae9ed4
cuesaws <132401293+cuesaws@users.noreply.github.com>
2024-01-12 08:49:40 -0500

    RH test AFT 01122024 v1

---
+++ terraform/AFT_Test_RH_01122024_1.tf
@@ -0,0 +1,24 @@
+/*module "AFT_Test_RH_01122024_1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+      AccountEmail              = "cuesaws+rhtest01122024@gmail.com"
+      AccountName               = "rhtest011220241"
+      ManagedOrganizationalUnit = "AFT (ou-dld9-73newpki)"
+      SSOUserEmail              = "rhaughton@cuesinc.com"
+      SSOUserFirstName          = "Ryan"
+      SSOUserLastName           = "Haughton"
+    }
+  account_tags = {
+    "application" = "AFT"
+    "business_region" = "us-east-1"
+    "owner" = "rhaughton@cuesinc.com"
+  }
+  
+  change_management_parameters = {
+    change_requested_by = "Ryan Haughton"
+    change_reason       = "AWS Control Tower Account Factory for Terraform test run 01122024 v1"
+  }
+
+  account_customizations_name = "sandbox"
+}*/
